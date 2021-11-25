0059b85cd7e215d675c91f2b3e770edb0a615497
Ruslan_Niadzvetski <ruslanned2001@gmail.com>
2021-11-25 20:38:13 +0300

    initial commit

---
+++ src/main/java/com/rusned/tr_task/TrTaskApplication.java
@@ -0,0 +1,13 @@
+package com.rusned.tr_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrTaskApplication.class, args);
+    }
+
+}
