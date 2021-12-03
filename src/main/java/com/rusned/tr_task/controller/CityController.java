c5df668ec9ef0178eb0e1f2f3f55e5b6044d7d29
Ruslan_Niadzvetski <ruslanned2001@gmail.com>
2021-12-03 07:51:23 +0300

    logback configuration added

--- src/main/java/com/rusned/tr_task/controller/CityController.java
+++ src/main/java/com/rusned/tr_task/controller/CityController.java
@@ -2,6 +2,8 @@ package com.rusned.tr_task.controller;
 
 import com.rusned.tr_task.domain.CityEntity;
 import com.rusned.tr_task.service.CityService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +14,7 @@ import java.util.List;
 @RequestMapping("/cities")
 public class CityController {
 
+    private static final Logger logger= LoggerFactory.getLogger(CityController.class);
     private final CityService cityService;
 
     @Autowired
@@ -26,6 +29,7 @@ public class CityController {
 
     @GetMapping("/{id}")
     public ResponseEntity<CityEntity> findById(@PathVariable(value = "id") Long id){
+        logger.info("Request to /cities/{id} endpoint (get method) passed");
         return ResponseEntity.ok(cityService.findById(id));
     }
 
